f3d8bff0a2a4af9fe6da2bc2e1b3975a05b96f58
John <johnsampson@uvic.ca>
2025-11-12 15:12:28 -0800

    update index.py to account for new function scoped app

--- index.py
+++ index.py
@@ -6,16 +6,16 @@ Simple index file container to serve the app at root instead of /Map_and_Tables_
 """
 
 # Import the main app
-from Map_and_Tables_App import layout as north_layout
+from Map_and_Tables_App import app_entrypoint as north_app_entrypoint
 from Metro_Van_App import layout as van_layout
 
 appver = os.environ.get("APP_VER", "NORTH")
 
 if appver == "NORTH":
-    layout = north_layout
+    app = north_app_entrypoint()
 else:
-    layout = van_layout
+    app = van_layout
 
 # Configure panel to serve at root
-pn.serve(layout, port=8080, address="0.0.0.0", allow_websocket_origin=["*"], 
+pn.serve(app, port=8080, address="0.0.0.0", allow_websocket_origin=["*"], 
          autoreload=True, show=True, title="Assessment Map Viewer")
