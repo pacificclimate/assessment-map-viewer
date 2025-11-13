e7f58b4fa7616c9ccbdec59fd8d842bad5f47b6d
John <johnsampson@uvic.ca>
2025-11-12 16:55:29 -0800

    tweak van version to use new app entry

--- index.py
+++ index.py
@@ -7,14 +7,14 @@ Simple index file container to serve the app at root instead of /Map_and_Tables_
 
 # Import the main app
 from Map_and_Tables_App import app_entrypoint as north_app_entrypoint
-from Metro_Van_App import layout as van_layout
+from Metro_Van_App import app_entrypoint as van_app_entrypoint
 
 appver = os.environ.get("APP_VER", "NORTH")
 
 if appver == "NORTH":
     app = north_app_entrypoint
 else:
-    app = van_layout
+    app = van_app_entrypoint
 
 # Configure panel to serve at root
 pn.serve(app, port=8080, address="0.0.0.0", allow_websocket_origin=["*"], 
